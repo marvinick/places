667e1e6d746b9d2c215d0a9c714e1ca59f9acaee
Jason Creighton & Marvin Nikijuluw <student@epicodus.com>
2015-08-05 15:26:56 -0700

    finished places twig

--- app/app.php
+++ app/app.php
@@ -9,52 +9,25 @@
 
   $app = new Silex\Application();
 
-  $app->get("/", function() {  //Accessing static methods and outputting saved list
-    $output = "";
-    $all_places = Place::getAll();
-    if (!empty($all_places)){
-      $output .= "
-      <h1>Places Visited</h1>
-      <p>Here are all the places you visited:</p>";
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+      'twig.path' => __DIR__.'/../views'
+  ));
 
-      foreach ($all_places as $place){  //foreach loop grabbing saved places
-        $output .= "<p>" . $place->getPlace() . "</p>";
-      }
-    }
+  $app->get("/", function() use ($app) {  //Accessing static methods and outputting saved list
 
-  $output .= "
-    <form action='/places' method='post'>
-      <label for='description'>Where have you been?</label>
-      <input id='description' name='description' type='text'>
-      <button type='submit'>Add place</button>
-    </form>"; //form for grabbing places visited
+    return $app['twig']->render('places.html.twig', array('places' => Place::getAll()));
 
-    $output.= "
-    <form action='/delete_places' method='post'>
-        <button type='submit'>delete</button>
-    </form>
-    ";  // form for deleting places
-
-    return $output;
   });
 
-  $app->post("/places", function(){ //posting info retrieved from place post
-    $place = new Place($_POST['description']);
+  $app->post("/places", function() use ($app){ //posting info retrieved from place post
+    $place = new Place($_POST['place']);
     $place->save();
-    return "
-    <h1>You created a destination</h1>
-    <p>" . $place->getPlace() . "</p>
-    <p><a href='/'>View your list of places you visited.</a></p>";
-
+    return $app['twig']->render('create_place.html.twig', array('newplace' => $place));
   });
 
-  $app->post("/delete_places", function() {  //delete function for stored places
+  $app->post("/delete_places", function() use ($app) {  //delete function for stored places
    Place::deleteAll();
-
-    return "
-      <h1>List Cleared!</h1>
-      <p><a href='/''>Home</a></p>
-    ";
+   return $app['twig']->render('delete_places.html.twig');
   });
 
   return $app;
